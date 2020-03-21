f8ffd0abbc69dcfbfc0fb6ac615c719b166d9e6b
Isaque Dias <isaquediasm@gmail.com>
2020-03-21 14:59:41 +0100

    create theme

---
+++ theme/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle, css } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle(
+  () => css`
+    @import url('https://fonts.googleapis.com/css?family=Mukta:300,400,500,700&display=swap');
+
+    body {
+      background: red;
+      font-family: 'Mukta', sans-serif !important;
+    }
+  `
+);
+
+export { GlobalStyle };
