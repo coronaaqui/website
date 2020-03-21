5859c4d17b185371e59ae63e0422d0ebcb80ea6d
Isaque Dias <isaquediasm@gmail.com>
2020-03-21 21:11:17 +0100

    fix

--- pages/estado/[state].js
+++ pages/estado/[state].js
@@ -1,18 +1,10 @@
 import { useRouter } from 'next/router';
-import { Banner } from '../../components/Banner';
-import { Footer } from '../../components/Footer';
 
 const State = () => {
   const router = useRouter();
   const { state } = router.query;
 
-  return (
-    <section>
-      <Banner />
-      Estado: {state}
-      <Footer />
-    </section>
-  );
+  return <section>Estado: {state}</section>;
 };
 
 export default State;
